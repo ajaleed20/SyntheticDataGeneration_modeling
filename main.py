df3b2868d8c48bf8136120e36f36a902dacde40e
ajaleed <ahsan_jaleed17@outlook.com>
2020-12-07 01:10:21 +0500

    redundant commented code omitted

--- main.py
+++ main.py
@@ -69,7 +69,6 @@ class App(tk.Frame):
             ctypes.windll.user32.MessageBoxW(0, str(e)+"\n\n Rerun Code to enter values correctly", "ERROR MESSAGE", 1)
             exit(0)
 
-
     def collectAnswers(self):
         self.answers['IndicatorBenchmarkValue'] = self.question1.get()
         self.answers['IndicatorGoodValue'] = self.question2.get()
@@ -86,7 +85,6 @@ class App(tk.Frame):
         ctypes.windll.user32.MessageBoxW(0, "Synthetic Random Data Generation Done!\n\n File Created at location:\n"+ str(ROOT_DIR), "Synthetic Data Generation", 1)
         exit(0)
 
-#
 def printAnswers(answers):
     print("Indicator Benchmark Value: ", answers['IndicatorBenchmarkValue'])
     print("Indicator Good Value: ", answers['IndicatorGoodValue'])
@@ -115,20 +113,18 @@ def printAnswers(answers):
         ctypes.windll.user32.MessageBoxW(0,str(e),"ERROR MESSAGE", 1)
         print(e)
         exit(0)
-    #"Date Format entered wrong: Rerun Code with following format\n\n 'yy-mm-dddd'"
-
 
     if answers['Granularity'] == 'd':
         SyntheticValuesCount = deltadate.days
-        level = GranularityLevel.one_day.value
+        level = GranularityLevel.one_day.value[0]
 
     elif answers['Granularity'] == 'h':
         SyntheticValuesCount = (deltadate.days) * 24
-        level = GranularityLevel.one_hour.value
+        level = GranularityLevel.one_hour.value[0]
 
     elif answers['Granularity'] == 'm':
         SyntheticValuesCount = (deltadate.days) * 24 * 60
-        level = GranularityLevel.one_minute.value
+        level = GranularityLevel.one_minute.value[0]
 
     elif answers['Granularity'] == 's':
         SyntheticValuesCount = (deltadate.days) * 24 * 60 * 60
@@ -136,15 +132,15 @@ def printAnswers(answers):
 
     elif answers['Granularity'] == 'tm':
         SyntheticValuesCount = (deltadate.days) * 24 * 6
-        level = GranularityLevel.ten_min.value
+        level = GranularityLevel.ten_min.value[0]
 
     elif answers['Granularity'] == 'ts':
         SyntheticValuesCount = (deltadate.days) * ((24 * 60) * 2)
         level = GranularityLevel.thirty_sec.value
 
     elif answers['Granularity'] == 'w':
-        SyntheticValuesCount = (deltadate.days) / 7
-        level = GranularityLevel.one_week.value
+        SyntheticValuesCount = math.floor((deltadate.days) / 7)
+        level = GranularityLevel.one_week.value[0]
 
     else:
         print("Granularity entered wrong: Rerun Code with following entry:\n\n"+
@@ -158,31 +154,21 @@ def printAnswers(answers):
         exit(0)
 
 
-    benchmarkrange = math.ceil(float(0.50 * SyntheticValuesCount))  # 60% values from within benchmark defined
-    limitrange     = math.ceil(int(0.30 * SyntheticValuesCount))   # 35% values from within low limit 1 & 2 defined
+    benchmarkrange = math.ceil(float(0.50 * SyntheticValuesCount))  # 50% values from within benchmark defined
+    limitrange     = math.ceil(int(0.30 * SyntheticValuesCount))   # 30% values from within low limit 1 & 2 defined
     outliersrange1  = int(0.05 * SyntheticValuesCount)   # 5% values as outliers or alarm values.
     outliersrange2 =  int(0.05 * SyntheticValuesCount)  # 5% values as outliers or alarm values.
-    replicate_range = int(0.10 * SyntheticValuesCount)
+    replicate_range = int(0.10 * SyntheticValuesCount)  # 10% values as outliers or alarm values.
 
     Generated_bm_Values = np.random.uniform(MinVal_bm, MaxVal_bm, benchmarkrange)
-    #Generated_bm_Values_dup = [np.random.randrange(MinVal_bm, MaxVal_bm) for i in range(benchmarkrange)]
-        #np.random.choice(MinVal_bm, MaxVal_bm, benchmarkrange, replace=True)
-
-
     Generated_lm_Values = np.random.uniform(float(answers['LowLimit2Value']), float(answers['LowLimit1Value']), limitrange)
     Generated_out1_Values = np.random.uniform(float(MaxVal_bm+1),float(MaxVal_bm+1.5) , outliersrange1)
     Generated_out2_Values = np.random.uniform(float(float(answers['LowLimit2Value'])-0.5), float(float(answers['LowLimit2Value']) - 1.5), outliersrange2)
 
 
-    #np.concatenate((a, b), axis=0)
-
     SyntheticData = np.concatenate((Generated_bm_Values, Generated_lm_Values, Generated_out1_Values, Generated_out2_Values), axis=None)
     df = pd.DataFrame(data=SyntheticData, columns=["Feature"])
 
-    # if SyntheticValuesCount == benchmarkrange + limitrange + outliersrange1 + outliersrange2:
-    #     SyntheticData = np.concatenate((Generated_bm_Values, Generated_lm_Values, Generated_out1_Values, Generated_out2_Values), axis=None)
-    #
-    # el
     if SyntheticValuesCount < (benchmarkrange + limitrange + outliersrange1 + outliersrange2):
         df.drop(df.tail(SyntheticValuesCount - (benchmarkrange + limitrange + outliersrange1 + outliersrange2)).index,inplace=True)
 
@@ -196,23 +182,12 @@ def printAnswers(answers):
 
     df_replicate = df.head(replicate_range)
     df = df.append(df_replicate, ignore_index=True)
- #   print("original dataframe:\n")
     print(df)
     df = shuffle(df)
-    print("shuffled data frame:\n")
+    print("shuffled data frame is as follows:\n")
     print(df)
-    #df_repeat = df.sample(n=len(df), replace=True)
-
-    #print(df_repeat)
-    #print(SyntheticData)
-    # print([item for item, count in collections.Counter(df).items() if count > 1])
-    # print(df.values)
-    # df.to_csv(answers['FileName']+'.csv' , index= False)
-    # print([item for item, count in collections.Counter(df).items() if count > 1])
 
     df['ts'] = pd.DataFrame({'ts': pd.date_range(start=sdate, end=edate, freq=get_freq_by_level(level))})
-
-
     df['ts'] = pd.to_datetime(df['ts'])
     df['ts'] = pd.to_datetime(df['ts']).dt.tz_localize(None)
     df = df.sort_values(by='ts')
@@ -222,72 +197,30 @@ def printAnswers(answers):
     series = read_csv(answers['FileName'] + '.csv', header=0, index_col=1)
     print(series.shape)
 
-    #print(series)
-    #X[0, :], X[1, :], c = y, cmap = plt.cm.Spectral
-    # pyplot.plot(series)
-    # pyplot.show()
-    #
-    # input_lead_lag()
-    # lead_time, lag_time = send_values(lead=0,lag=0)
-
-    # choice = easygui.enterbox("Enter Machine Learning Model choice:\n 1:MultiLayerPerceptron\n 2:Convolutional Neural Network\n 3:CNN_with_HyperParameterTuning\n 4:MLP_with_HyperParameterTuning\n Default:MultiLayerPerceptron")
-    # # choice = input("Enter choice:\n 1:mlp\n 2:cnn\n 3:cnn_hp\n 4:mlp_hp")
-    # print("User Choice is: " + choice)
-
     forecast, model, rmse_list, rmse = executeRegression(InputEnums.input.value,series,
                                                                        InputEnums.lag_time_steps.value, InputEnums.lead_time_steps.value, InputEnums.test_train_split_size.value,
                                                                        InputEnums.confidence_interval_multiple_factor.value)
 
     print("end")
 
-    #series = read_csv('monthly-car-sales.csv', header=0, index_col=0)
-
-
-
-def send_values(lead,lag):
-    return lead,lag
-#
-# def train_test_split(data, n_test):
-# 	return data[:-n_test], data[-n_test:]
-
-#
-#
-# def 1_series_to_supervised(data, n_in=1, n_out=1):
-# 	df = DataFrame(data)
-# 	cols = list()
-# 	# input sequence (t-n, ... t-1)
-# 	for i in range(n_in, 0, -1):
-# 		cols.append(df.shift(i))
-# 	# forecast sequence (t, t+1, ... t+n)
-# 	for i in range(0, n_out):
-# 		cols.append(df.shift(-i))
-# 	# put it all together
-# 	agg = concat(cols, axis=1)
-# 	# drop rows with NaN values
-# 	agg.dropna(inplace=True)
-# 	return agg.values
-
-
-def get_freq_by_level(ganuality_level_value):
-    if GranularityLevel.one_hour.value[0] == ganuality_level_value[0]:
-        return '60T'
-    elif GranularityLevel.three_hour.value[0] == ganuality_level_value[0]:
-        return '180T'
-    elif GranularityLevel.one_day.value[0] == ganuality_level_value[0]:
-        return '1440T'
-    elif GranularityLevel.one_week.value[0] == ganuality_level_value[0]:
-        return '10080T'
-    elif GranularityLevel.ten_min.value[0] == ganuality_level_value[0]:
-        return '10T'
-    elif GranularityLevel.one_minute.value[0] == ganuality_level_value[0]:
-        return '1T'
-    elif GranularityLevel.one_sec.value[0] == ganuality_level_value[0]:
-        return '0.016666666667T'
-    elif GranularityLevel.thirty_sec.value[0] == ganuality_level_value[0]:
-        return '0.5T'
-
-
-
+def get_freq_by_level(granularity_level_value):
+    if GranularityLevel.one_hour.value[0] == granularity_level_value:
+           return '60T'
+    elif GranularityLevel.three_hour.value[0] == granularity_level_value:
+           return '180T'
+    elif GranularityLevel.one_day.value[0] == granularity_level_value:
+           return '1440T'
+    elif GranularityLevel.one_week.value[0] == granularity_level_value:
+           return '10080T'
+    elif GranularityLevel.ten_min.value[0] == granularity_level_value:
+           return '10T'
+    elif GranularityLevel.one_minute.value[0] == granularity_level_value:
+           return '1T'
+    #elif GranularityLevel.one_sec.value[0] == ganuality_level_value[0]:
+    elif GranularityLevel.one_sec.value == granularity_level_value:
+           return '0.016666666667T'
+    elif GranularityLevel.thirty_sec.value == granularity_level_value:
+           return '0.5T'
 
 
 def generatedata(answers):
